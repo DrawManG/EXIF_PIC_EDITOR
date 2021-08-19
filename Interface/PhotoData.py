c0899e5bbb776ce6af222269ce51d0147ebf58bd
Дмитрий Смирнов <we-are-legion2013@yandex.ru>
2021-08-19 18:01:29 +0300

    поставка ограничителей, размерность штампа, фикс кнопок режимов и многое другое

--- Interface/PhotoData.py
+++ Interface/PhotoData.py
@@ -1,9 +1,11 @@
 
 from sys import path
+
+from PyQt5.QtGui import QIntValidator
 from Module.mega_script import mega_script
 from Module.make_photo_array import make_photo_array
 from Module.read_xls import read_xls
-from PyQt5.QtWidgets import QMainWindow, QApplication, QRadioButton, QWidget, QLineEdit, QFileDialog, QPushButton, QGridLayout, QMessageBox, QLabel, QComboBox
+from PyQt5.QtWidgets import QHBoxLayout, QMainWindow, QApplication, QRadioButton, QVBoxLayout, QWidget, QLineEdit, QFileDialog, QPushButton, QGridLayout, QMessageBox, QLabel, QComboBox
 import os
 import PIL
 
@@ -18,33 +20,42 @@ class PhotoData(QWidget):
         self.photos_dir = ""
         self.names = []  
         self._create_UI()
-        self.mode = 0
+        self.mode = 5
         self.button_open_excel.clicked.connect(self.open_excel)
         self.button_dir_photo.clicked.connect(self.choose_photo_dir)
         self.button_process.clicked.connect(self.proccessing)
         self.switch_mode_EXIF.clicked.connect(self.switch_modeE)
         self.switch_mode_XLS.clicked.connect(self.switch_modeX)
+        self.combo.activated.connect(self.clean_combo)
 
-
+    
+        
     def _create_UI(self):
+
         self.layout = QGridLayout(self)
         self.button_open_excel = QPushButton("Открыть файл excel")
         self.excel_path = QLineEdit()
         self.excel_path.setEnabled(False)
         self.combo = QComboBox()
         self.combo.addItems(["Стабилометр", "Срез", "Компрессия","Консолидация"])
-        self.len_datas = QLabel("Найдено образцов:   ")
+        self.len_datas = QLabel("Найдено образцов: ")
         self.button_dir_photo = QPushButton("Выбрать папку с фото")
         self.photo_path = QLineEdit()
         self.photo_path.setEnabled(False)
         self.len_photo = QLabel("Найдено фото: ")
         self.button_process = QPushButton("Перебить даты")
         self.lbl_fontsize = QLabel("Размер шрифта: ")
-        self.lineedit_fontsize = QLineEdit()
+        self.lineedit_fontsize = QLineEdit("25")
+        self.lineedit_fontsize.setValidator(QIntValidator())
         self.lbl_switch = QLabel("Режимы:")
         self.switch_mode_XLS = QRadioButton("XLS")
         self.switch_mode_EXIF = QRadioButton("EXIF")
-        self.switch_mode_XLS.setChecked(True)
+        #self.switch_mode_XLS.setChecked(True)
+
+        self.aggregation = QHBoxLayout()
+        self.aggregation.addWidget(self.lbl_switch)
+        self.aggregation.addWidget(self.switch_mode_XLS)
+        self.aggregation.addWidget(self.switch_mode_EXIF)
         
 
         self.layout.addWidget(self.button_open_excel, 0, 0)
@@ -57,23 +68,61 @@ class PhotoData(QWidget):
         self.layout.addWidget(self.button_process, 3, 0)
         self.layout.addWidget(self.lbl_fontsize,2,0)
         self.layout.addWidget(self.lineedit_fontsize,2,1)
-        self.layout.addWidget(self.lbl_switch,2,2)
-        self.layout.addWidget(self.switch_mode_XLS,2,3)
-        self.layout.addWidget(self.switch_mode_EXIF,2,4)
+        self.layout.addLayout(self.aggregation,2,2)
 
     def switch_modeE(self):
         self.button_open_excel.setEnabled(False)
         self.combo.setEnabled(False)
         self.len_datas.setEnabled(False)
-        self.mode = 0
+ 
     
     def switch_modeX(self):
         self.button_open_excel.setEnabled(True)
         self.combo.setEnabled(True)
         self.len_datas.setEnabled(True)
-        self.mode = 1
+
+    def clean_combo(self):
+        try:
+            #self.photos = []
+            excel_file = self.excel_path.text()
+            self.len_datas.setText("Найдено образцов: ")
+            self.excel_path.setText("")
+            self.dates = [] 
+            if excel_file and excel_file.endswith('.xls'):
+                self.names, self.dates =  read_xls.read_xls(excel_file, self.combo.currentText())
+                self.excel_path.setText(excel_file)
+                self.len_datas.setText("Найдено образцов: " + str(len(self.dates)))
+        except:
+                m = QMessageBox()
+                m.setText("То, что вы выбрали, нету в Excel! Посмотрите Excel и сверьте с выбором.")
+                m.exec()
+                self.excel_path.setText(excel_file)
+                self.combo.setCurrentIndex(0)
+                self.clean_combo()
+        
+    def clean_DataBase(self):
+        self.len_datas.setText("Найдено образцов: ")
+        self.len_photo.setText("Найдено фото: ")
+        self.excel_path.setText("")
+        self.photo_path.setText("")
+        self.dates = [] 
+        self.photos = []  
+        self.photos_dir = ""
+        self.names = []  
+        self.mode = 5
 
 
+    def cleaner(self):
+        super().__init__()
+        self.len_datas.setText("Найдено образцов: ")
+        self.len_photo.setText("Найдено фото: ")
+        self.excel_path.setText("")
+        self.photo_path.setText("")
+        self.dates = [] 
+        self.photos = []  
+        self.photos_dir = ""
+        self.names = []  
+        self.mode = 5
 
 
 
@@ -95,46 +144,114 @@ class PhotoData(QWidget):
             self.len_photo.setText("Найдено фото: " + str(len(self.photos)))
 
     def proccessing(self):
+        if self.switch_mode_XLS.isChecked():
+            self.mode = 1
+        elif self.switch_mode_EXIF.isChecked():
+            self.mode = 0
+
+        try:
+            if not self.lineedit_fontsize.text() == "":
+                if int(self.lineedit_fontsize.text()) > 50 or int(self.lineedit_fontsize.text()) < 10:
+                    m = QMessageBox()
+                    m.setText("У вас не правильно выставлен размер шрифта, он начинается от 10 и заканчивается 50. Если у вас его нет, то поставьте")
+                    m.exec()
+                    return
+            else: 
+                m = QMessageBox()
+                m.setText("Введите размер шрифта")
+                m.exec()
+                return
+        except: 
+                    m = QMessageBox()
+                    m.setText("УФ, не учёл такой момент,ставлю 25 шрифт, шрифт ставится от 10 до 50")
+                    m.exec()
+                    self.lineedit_fontsize.setText(str(25))
+                    
+        
         if len(self.photos) == 0:
             m = QMessageBox()
             m.setText("Отсутствуют фото")
             m.exec()
+            return
         elif len(self.dates) == 0 and self.mode == 1:
             m = QMessageBox()
             m.setText("Отсутствуют образцы")
             m.exec()
-        elif self.mode == 0 :
+            return
+        elif self.mode == 0:
+
             print("exif")
             self.dates = self.exif_dates()
             save_path = self.photos_dir + "/modified/"
             print('dates  ', self.dates)
+            m = QMessageBox()
             if not os.path.exists(save_path):
                 os.mkdir(save_path)
-            mega_script.mega_script(save_path, self.photos, self.names, self.dates)
-            m = QMessageBox()
+            try:
+                mega_script.mega_script(save_path, self.photos, self.names, self.dates,self.lineedit_fontsize.text())
+            except Exception as Error:
+                print("Вы засунули фото с EXIF, которые уже были обработаны или это не фотография с камеры! пересмотрите папку с фотографиями. Подробнее: ",Error)
+                m.setText("Вы засунули фото с EXIF, которые уже были обработаны или это не фотография с камеры! Пересмотрите папку с фотографиями. Подробнее: "+ str(Error))
+                m.exec()
+            
             m.setText("Готово")
             m.exec()
-        else:
+            old_path = self.excel_path.text()
+            old_path_p = self.photo_path.text()
+            self.switch_mode_XLS.setChecked(True)
+            self.button_open_excel.setEnabled(True)
+            self.combo.setEnabled(True)
+            self.len_datas.setEnabled(True)
+            self.cleaner()
+            self.excel_path.setText(old_path)
+            self.clean_combo()
+            
+            self.switch_mode_EXIF.setChecked(True)
+            self.button_open_excel.setEnabled(False)
+            self.combo.setEnabled(False)
+            self.len_datas.setEnabled(False)
 
+
+
+
+
+
+        else:
+            if len(self.dates) < len(self.photos):
+                m = QMessageBox()
+                m.setText("У вас в EXCEL ячеек меньше, чем фотографий, исправьте пожалуйста!")
+                m.exec()
+                return
             save_path = self.photos_dir + "/modified/"
             if not os.path.exists(save_path):
                 os.mkdir(save_path)
-            mega_script.mega_script(save_path, self.photos, self.names, self.dates)
+            mega_script.mega_script(save_path, self.photos, self.names, self.dates,self.lineedit_fontsize.text())
             m = QMessageBox()
             m.setText("Готово")
             m.exec()
+            old_path = self.excel_path.text()
+            old_path_p = self.photo_path.text()
+            self.switch_mode_XLS.setChecked(True)
+            self.button_open_excel.setEnabled(True)
+            self.combo.setEnabled(True)
+            self.len_datas.setEnabled(True)
+            self.cleaner()
+            self.excel_path.setText(old_path)
+            self.clean_combo()
 
     def exif_dates(self):
-
-        i_img = 0
-        base = []
-
-        while i_img < len(self.photos):
-            image_now = PIL.Image.open(self.photos[i_img])
-            hook_date = str(image_now._getexif()[36867])
-            year , house = hook_date.split("0")
-            year.replace(":",".")
-            base.append(year," ",house)
-            i_img+=1
-        print("base ",base)
-        return base
+        try:
+            data_base = []
+            i=0
+            while i < len(self.photos):
+                img = PIL.Image.open(self.photos[i])
+                exif_full = img._getexif()[36868]
+                year_mouth_day,house_minute_second = str(exif_full).split(' ')
+                year_mouth_day = year_mouth_day.replace(":",".")
+                data_base.append(year_mouth_day+" "+house_minute_second)
+                i+=1
+            print('dates  ',self.dates)
+            return data_base
+        except Exception as Error:
+            print("В списке картинок, есть картинка, которая не имеет Exif. Подробнее: ",Error)
+            
--- Module/edit_photo.py
+++ Module/edit_photo.py
@@ -6,7 +6,7 @@ from datetime import datetime,timedelta
 from Base_info.Data_base import datacenter_coordinates,random_coordinates_X,random_coordinates_Y,arial_font_path,bad_Cordinates
 
 class edit_photo:
- def edit_photo(save_path,photo_path,name,data):
+ def edit_photo(save_path,photo_path,name,data,lineedit_fontsize):
      i = 0
      while i <= len(photo_path) - 1 :
         print("Сейчас идёт файл:  " + name[i])
@@ -14,7 +14,8 @@ class edit_photo:
         w, h = im.size
         im = Image.open(photo_path[i])
         clac = int((h*100)/w * 10)
-        size = int(150/clac*100)
+        
+        size = int(lineedit_fontsize)
         draw_text = ImageDraw.Draw(im)
         font = ImageFont.truetype(arial_font_path, size= size * 2 , encoding="utf-8")
         time_rl = data[i].split(' ')[1]
@@ -22,39 +23,34 @@ class edit_photo:
         rus_date = ['','янв.', 'фев.', 'мар.', 'апр.', 'май', 'июн.', 'июл.', 'авг.', 'сен.', 'окт.', 'ноя.', 'дек.']
         print(str(d) + " " + str(rus_date[int(m)]) + " " + str(y) + " " + "г.," + " " + str(time_rl))
         random_last = random.randint(2, 3)
-        if w == 1920 and h == 1080:
-            w = w - 400
-            h = h - 400
-        draw_text.text(
-            (w - (w/4.25) +2, h - (h - 10)),
-            str(d) + " " + str(rus_date[int(m)]) + " " + str(y) + " " + "г.," + " " + str(time_rl),
-            font=font,
-            fill='black')
-        draw_text.text(
-            (w - (w/4.25) , h - (h - 10)),
-            str(d) + " " + str(rus_date[int(m)]) + " " + str(y) + " " + "г.," + " " + str(time_rl),
-            font=font,
-            fill='white')
-        draw_text.text(
-            (w - (w/4.25)+2, h - (h - 70)),
-            bad_Cordinates + str(random_last) + '"',
-            font=font,
-            fill='black')
-        draw_text.text(
-            (w - (w/4.25), h - (h  - 70)),
-            bad_Cordinates + str(random_last) + '"',
-            font=font,
-            fill='white')
-        draw_text.text(
-            (w - (w/4.25)+2, h - (h  - 130)),
-            datacenter_coordinates,
-            font=font,
-            fill='black')
-
-        draw_text.text(
-            (w - (w/4.25), h - (h  - 130)),
-            datacenter_coordinates,
-            font=font,
-            fill='white')
+        if w>h:
+            if w == 1920 and h == 1080:
+                w = w - 400
+                h = h - 400
+            draw_text.text(
+                (w - (w/3)-(size*30/100)  , h - (h - size - 5 )),
+                str(d) + " " + str(rus_date[int(m)]) + " " + str(y) + " " + "г.," + " " + str(time_rl) +"\n"+bad_Cordinates + str(random_last) + '"' + "\n" + datacenter_coordinates,
+                font=font,
+                fill='black')
+            draw_text.text(
+                (w - (w/3)-(size*30/100)+2, h - (h - size - 5)),
+                str(d) + " " + str(rus_date[int(m)]) + " " + str(y) + " " + "г.," + " " + str(time_rl) +"\n"+bad_Cordinates + str(random_last) + '"' + "\n" + datacenter_coordinates,
+                font=font,
+                fill='white')
+        else:
+            if w == 1920 and h == 1080:
+                w = w - 400
+                h = h - 400
+            draw_text.text(
+                (w - (w/3)-(size*500/70)  , h - (h - size - 5 )),
+                str(d) + " " + str(rus_date[int(m)]) + " " + str(y) + " " + "г.," + " " + str(time_rl) +"\n"+bad_Cordinates + str(random_last) + '"' + "\n" + datacenter_coordinates,
+                font=font,
+                fill='black')
+            draw_text.text(
+                (w - (w/3)-(size*500/70)+2, h - (h - size - 5)),
+                str(d) + " " + str(rus_date[int(m)]) + " " + str(y) + " " + "г.," + " " + str(time_rl) +"\n"+bad_Cordinates + str(random_last) + '"' + "\n" + datacenter_coordinates,
+                font=font,
+                fill='white')
+        
         im.save(str(save_path) + '/' + name[i] + '.jpg')
         i = i + 1
